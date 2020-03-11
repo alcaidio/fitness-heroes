42f6f661a3050c63647bdf3b78348fd865d70528
Timothy Alcaide <timothy@mbp-de-timothy-1.home>
2020-03-11 02:03:00 +0100

    build(*): build the app and add lite-server for test in dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  // IMPORTANT !
+  // Here is the same bdd than dev
+  // In real project you need to change credentials with the prod bdd
+  firebase: {
+    apiKey: "AIzaSyBHYtYPXX_qxy8KD1v5mAq_qew_mC267OE",
+    authDomain: "ng-fitness-tracker-9c299.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-9c299.firebaseio.com",
+    projectId: "ng-fitness-tracker-9c299",
+    storageBucket: "ng-fitness-tracker-9c299.appspot.com",
+    messagingSenderId: "692198951717",
+    appId: "1:692198951717:web:a4514dbfe89367abd0e8cc",
+    measurementId: "G-JEHGGXSDV1"
+  }
 };
