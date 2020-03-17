477cc1159f4be7ce945d322a1be07e3958f46762
Timothy Alcaide <timothy@mbp-de-timothy-1.home>
2020-03-17 22:29:08 +0100

    feat(training): add actions and reducer

---
+++ src/app/training/store/training.reducer.ts
@@ -0,0 +1,38 @@
+import * as fromRoot from '../../app.reducer';
+import { Exercise } from './../exercise.model';
+import * as fromTraining from './training.actions';
+
+export interface TrainingState {
+    availableExercises: Exercise[],
+    finishedExercises: Exercise[],
+    activeTraining: Exercise
+}
+
+export interface State extends fromRoot.State {
+    training: TrainingState
+}
+
+const initialState: TrainingState = {
+    availableExercises: [],
+    finishedExercises: [],
+    activeTraining: null
+}
+
+export function trainingReducer(state = initialState, action: fromTraining.TrainingAction) {
+    switch (action.type) {
+        case fromTraining.SET_AVAILABLE_TRAININGS:
+            return { ...state, availableExercises: action.payload }
+        case fromTraining.SET_FINISHED_TRAININGS:
+            return { ...state, finishedExercises: action.payload }
+        case fromTraining.START_TRAINING:
+            return { ...state, activeTraining: action.payload }
+        case fromTraining.STOP_TRAINING:
+            return { ...state, activeTraining: null }
+        default:
+            return state
+    }
+}
+
+export const getAvailableExercises = (state: TrainingState) => state.availableExercises
+export const getFinishedExercises = (state: TrainingState) => state.finishedExercises
+export const getActiveTraining = (state: TrainingState) => state.activeTraining
